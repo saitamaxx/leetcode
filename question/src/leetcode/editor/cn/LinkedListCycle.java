98bc4348ee9fbdba4a0d6aeb16a9a22a112ac727
sunyu <sunyu@fenxianglife.com>
2022-08-31 23:11:03 +0800

    提交

---
+++ question/src/leetcode/editor/cn/LinkedListCycle.java
@@ -0,0 +1,36 @@
+package leetcode.editor.cn;
+
+public class LinkedListCycle{
+    public static void main(String[] args) {
+        Solution solution = new LinkedListCycle().new Solution();
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
