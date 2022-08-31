bae161b39595bb68134cca534da73b5617c0c58f
sunyu <sunyu@fenxianglife.com>
2022-08-31 22:28:44 +0800

    删除链表的倒数第N个结点

--- question/src/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
+++ question/src/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -18,7 +18,25 @@ public class RemoveNthNodeFromEndOfList{
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode x = findFromEnd(dummy, n + 1);
+        x.next = x.next.next;
+        return dummy.next;
+    }
 
+    ListNode findFromEnd(ListNode head, int n) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        int k = 1;
+        while (p1 != null) {
+            if (k > n) {
+                p2 = p2.next;
+            }
+            p1 = p1.next;
+            k++;
+        }
+        return p2;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
